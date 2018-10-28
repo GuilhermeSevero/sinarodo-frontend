d81a0716dd3712d8a21e5a2f1188d91f54038367
Guilherme Severo <guilhermehmsevero@hotmail.com>
2018-10-27 23:19:32 -0300

    commit inicial do projeto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import lang from 'quasar-framework/i18n/pt-br'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}, i18n: lang
+})
 
 Vue.config.productionTip = false
 
